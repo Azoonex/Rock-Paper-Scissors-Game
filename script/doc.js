4c5fafce0c77586d73b599dc43fe60fabc18b8da
azoonex <amirjabic@gamil.com>
2024-12-13 14:02:44 -0800

    error

--- script/doc.js
+++ script/doc.js
@@ -1,9 +1,31 @@
-var demo = document.getElementById('demo');
+var demo = document.getElementById("demo");
+var winer = null;
+// img = > 1 paper / sclassors / rock
+var pattern = [
+    {
+        id: 1,
+        value: "paper",
+    },
+    {
+        id: 2,
+        value: "rock",
+    },
+    {
+        id: 3,
+        value: "sclssores",
+    },
+];
+function selectionBot() {
+    return Math.floor(Math.random()) * 3 + 1;
+}
 window.onload = function () {
-    for (var i = 1; i < 4; i++) {
-        var createIamge = document.createElement('img');
+    var botSelection = selectionBot();
+    for (var i = ; i < 6; i++) {
+        var createIamge = document.createElement("img");
         createIamge.id = i.toString();
         createIamge.src = "images-".concat(i, ".png");
         demo.appendChild(createIamge);
+        var selected = pattern[i].id;
+        console.log(i);
     }
 };
--- script/doc.ts
+++ script/doc.ts
@@ -1,12 +1,38 @@
-const demo = document.getElementById('demo')!;
+const demo = document.getElementById("demo")!;
+
+let winer: "user" | "bot" | null = null;
+// img = > 1 paper / sclassors / rock
+
+let pattern = [
+  {
+    id: 1,
+    value: "paper",
+  },
+  {
+    id: 2,
+    value: "rock",
+  },
+  {
+    id: 3,
+    value: "sclssores",
+  },
+];
+
+function selectionBot() {
+  return Math.floor(Math.random()) * 3 + 1;
+}
 
 window.onload = function () {
-    for (let i = 1;
-        i < 4; i++) {
-        const createIamge = document.createElement('img')!
-        createIamge.id = i.toString()
-            createIamge.src = `images-${i}.png`
+  const botSelection = selectionBot();
 
-        demo.appendChild(createIamge)
-    }
+  for (let i = ; i < 6; i++) {
+    const createIamge = document.createElement("img")!;
+    createIamge.id = i.toString();
+    createIamge.src = `images-${i}.png`;
+    demo.appendChild(createIamge);
+    let selected = pattern[i].id;
+
+    console.log(i)
+  }
 }
+
