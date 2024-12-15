17353caea9b4c959a2ef3e74ecd8fc9f06b48e01
azoonex <amirjabic@gmail.com>
2024-12-15 09:10:11 -0800

    to the end fix

--- script/doc.js
+++ script/doc.js
@@ -1,4 +1,6 @@
 var demo = document.getElementById("demo");
+var btnStart = document.getElementById("btn-start");
+var winderGame = document.getElementsByTagName("h2")[0];
 var winer = null;
 // img = > 1 paper / sclassors / rock
 var pattern = [
@@ -15,17 +17,58 @@ var pattern = [
         value: "sclssores",
     },
 ];
-function selectionBot() {
+function randomValue() {
     return Math.floor(Math.random()) * 3 + 1;
 }
-window.onload = function () {
-    var botSelection = selectionBot();
-    for (var i = ; i < 6; i++) {
+window.addEventListener("load", startGame);
+function startGame() {
+    // if (winer) {
+    //   return;
+    // }
+    var _loop_1 = function (i) {
         var createIamge = document.createElement("img");
-        createIamge.id = i.toString();
-        createIamge.src = "images-".concat(i, ".png");
+        createIamge.src = "images-".concat(i + 1, ".png");
         demo.appendChild(createIamge);
-        var selected = pattern[i].id;
-        console.log(i);
+        createIamge.addEventListener("click", function () {
+            return handleSetGameWiner(pattern[i].id, randomValue());
+        });
+    };
+    for (var i = 0; i < 3; i++) {
+        _loop_1(i);
     }
-};
+}
+function handleSetGameWiner(pattern, randomNumber) {
+    if (pattern !== randomNumber) {
+        console.log("user" + pattern, "bot" + randomNumber);
+        console.log(winer);
+        return;
+    }
+    if (typeof winer !== null) {
+        switch (pattern) {
+            case 1:
+                if (randomNumber === 3) {
+                    winer = "user";
+                }
+                else {
+                    winer = "bot";
+                }
+                break;
+            case 2:
+                if (randomNumber === 1) {
+                    winer = "user";
+                }
+                else {
+                    winer = "bot";
+                }
+                break;
+            case 3:
+                if (randomNumber === 3) {
+                    winer = "user";
+                }
+                else {
+                    winer = "bot";
+                }
+        }
+    }
+    winderGame.innerHTML = winer ? "winer" + winer : "winder  ";
+}
--- script/doc.ts
+++ script/doc.ts
@@ -1,4 +1,6 @@
 const demo = document.getElementById("demo")!;
+const btnStart = document.getElementById("btn-start")!;
+const winderGame = document.getElementsByTagName("h2")[0]!;
 
 let winer: "user" | "bot" | null = null;
 // img = > 1 paper / sclassors / rock
@@ -18,21 +20,60 @@ let pattern = [
   },
 ];
 
-function selectionBot() {
+function randomValue() {
   return Math.floor(Math.random()) * 3 + 1;
 }
 
-window.onload = function () {
-  const botSelection = selectionBot();
+window.addEventListener("load", startGame);
 
-  for (let i = ; i < 6; i++) {
+function startGame() {
+  // if (winer) {
+  //   return;
+  // }
+
+  for (let i = 0; i < 3; i++) {
     const createIamge = document.createElement("img")!;
-    createIamge.id = i.toString();
-    createIamge.src = `images-${i}.png`;
+    createIamge.src = `images-${i + 1}.png`;
     demo.appendChild(createIamge);
-    let selected = pattern[i].id;
 
-    console.log(i)
+    createIamge.addEventListener("click", () =>
+      handleSetGameWiner(pattern[i].id, randomValue())
+    );
   }
 }
+
+function handleSetGameWiner(pattern: number, randomNumber: number) {
+
+  if (pattern !== randomNumber) {
+    console.log("user" + pattern,"bot" + randomNumber);
+    console.log(winer);
+    return;
+  }
+  if (typeof winer !== null) {
+    switch (pattern) {
+      case 1:
+        if (randomNumber === 3) {
+          winer = "user";
+        } else {
+          winer = "bot";
+        }
+        break
+      case 2:
+        if (randomNumber === 1) {
+          winer = "user";
+        } else {
+          winer = "bot";
+        }
+        break
+      case 3:
+        if (randomNumber === 3) {
+          winer = "user";
+        } else {
+          winer = "bot";
+        }
+    }
+  }
+  winderGame.innerHTML = winer ? "winer" + winer : "winder  " 
+}
+
 
